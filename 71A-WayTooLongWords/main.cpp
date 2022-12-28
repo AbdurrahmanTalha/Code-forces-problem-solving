971ab2338c13787ad9cb2ee8accc3871865f46cb
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2022-12-28 21:54:50 +0600

    solved 71A-WayTooLongWords problem

--- 71A-WayTooLongWords/main.cpp
+++ 71A-WayTooLongWords/main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    int n;
+    scanf("%d", &n);
+
+    while (n--) {
+        string str;
+        cin >> str;
+
+        if (str.length() > 10) {
+            int count = str.length() - 2;
+            cout << str[0] << count << str[str.length() - 1] << endl;
+        } else {
+            cout << str << endl;
+        }
+    }
 
     return 0;
 }
