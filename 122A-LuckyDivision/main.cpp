e76ed58ee36eeb14def5ab1edf608dfb485d521c
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-01-16 00:32:49 +0600

    Solved problem Lucky Division

---
+++ 122A-LuckyDivision/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+void solve() {
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    scanf("%d", &n);
+
+    if (n % 4 == 0 || n % 7 == 0 || n % 44 == 0 || n % 47 == 0 || n % 74 == 0 || n % 77 == 0 || n % 447 == 0 || n % 474 == 0 || n % 477 == 0 || n % 744 == 0 || n % 747 == 0 || n % 774 == 0 || n % 774 == 0 || n % 777 == 0) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+
+    return 0;
+}
